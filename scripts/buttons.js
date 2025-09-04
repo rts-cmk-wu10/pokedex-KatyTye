9a94690fe8fb4d245bb878c79d09d5175b2f8abe
David Majholt <d.boy.dragoz@gmail.com>
2025-09-04 09:40:40 +0200

    Add button functionality and script for navigation in PokeDex

---
+++ scripts/buttons.js
@@ -0,0 +1,8 @@
+const buttons = document.querySelectorAll("#wrapper > .design button")
+
+buttons.forEach((element) => {
+
+	element.addEventListener("click", () => {
+		(element.innerHTML === "Pokemons") ? location.href = "index.html" : location.href = `${element.innerHTML.toLowerCase()}.html`;
+	})
+})
