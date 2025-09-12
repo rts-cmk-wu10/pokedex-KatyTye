eeee69f7185af8079651b44be2e2baa1ca9d8f96
David Majholt <d.boy.dragoz@gmail.com>
2025-09-12 13:49:05 +0200

    Removed Not Needed Files

--- script/urlHandler.js
+++ script/urlHandler.js
@@ -1,5 +1,9 @@
 const urlParams = new URLSearchParams(window.location)
-let searchParam = urlParams.get("search") || "1"
+let searchParam = urlParams.get("search")
+
+if (!searchParam) {
+	window.location = "index.html?search=pokemon-1"
+}
 
 if (searchParam.includes("pokemon")) {
 	loadSpeficPokemon(Number(searchParam.split("-")[1]))
